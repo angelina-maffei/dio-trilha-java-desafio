a81f2253bf6be786ecdd6dd243d34f26b0617354
angelina-maffei <angelina.maffei84@gmail.com>
2024-08-12 17:36:40 -0300

    desafio-dio-simulando-uma-conta-bancaria-atraves-do-terminal

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Por Favor, Digite o Número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+
+        System.out.print("Por Favor, Digite o Número da Agência: ");
+        String agencia = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Por Favor, Digite o Nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Por Favor, Digite o Saldo: ");
+        Double saldo = scanner.nextDouble();
+        System.out.println();
+        
+        scanner.close();
+
+        System.out.println();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+         + agencia + ", conta " + numeroConta + " e seu saldo " + String.format("%.2f", saldo) + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+
+        System.out.println();
+
     }
+
 }
